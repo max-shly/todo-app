d7f85845e699b549703cb1e49790dfb3b90677e8
max-shly <maxschly@gmail.com>
2026-02-23 15:10:08 +0300

    test: проверка husky хуков

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,13 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierConfig from './.prettierrc' assert { type: 'json' };
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -18,6 +22,17 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx,json,md,css,less}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      'prettier/prettier': ['error', prettierConfig],
     },
   },
 ])
