4a9e678ffde227a0fee0050984931f3abfc94ed6
max-shly <maxschly@gmail.com>
2026-03-01 18:38:34 +0300

    feat: add todo state and reducer

---
+++ src/store/reducers/todosReducer.ts
@@ -0,0 +1,43 @@
+import { type TodosState, type TodosAction } from '../../types/index.ts';
+
+export const initialState: TodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+export function todosReducer(state: TodosState, action: TodosAction) {
+  switch (action.type) {
+    case 'FETCH_START': {
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+    }
+    case 'FETCH_SUCCESS': {
+      return {
+        ...state,
+        todos: action.payload,
+        isLoading: false,
+        error: null,
+      };
+    }
+    case 'FETCH_ERROR': {
+      return {
+        ...state,
+        todos: [],
+        isLoading: false,
+        error: action.payload,
+      };
+    }
+    case 'SET_TODOS': {
+      return {
+        ...state,
+        todos: action.payload,
+        isLoading: false,
+        error: null,
+      };
+    }
+  }
+}
