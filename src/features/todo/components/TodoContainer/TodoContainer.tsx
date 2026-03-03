8e8143ae9c77426066a6c1250992b1010b923259
max-shly <maxschly@gmail.com>
2026-03-04 02:54:04 +0300

    feat: integrate TodoModal with useTodoModal

--- src/features/todo/components/TodoContainer/TodoContainer.tsx
+++ src/features/todo/components/TodoContainer/TodoContainer.tsx
@@ -1,34 +1,52 @@
 import { Button } from '@/shared/ui/Button/Button';
 
+import { useTodoModal } from '../../hooks/useTodoModal';
 import { useTodos } from '../../hooks/useTodos';
 import { TodoHeader } from '../TodoHeader/TodoHeader';
 import { TodoList } from '../TodoList/TodoList';
+import { TodoModal } from '../TodoModal/TodoModal';
 
 import styles from './TodoContainer.module.less';
 
 export function TodoContainer() {
-  const { state: todosState } = useTodos();
+  const { state: todosState, addTodo } = useTodos();
+  const { isOpen: isOpenModal, editingTodo, open: openModal, close: closeModal } = useTodoModal();
+
+  const handleSave = (data: { title: string; date: string }) => {
+    if (editingTodo) {
+      // test
+    } else {
+      addTodo({ ...data, status: 'incomplete' });
+    }
+
+    closeModal();
+  };
 
   return (
-    <div className={styles.container}>
-      <TodoHeader
-        onAddTodo={() => {}}
-        onDeleteAllTodos={() => {}}
-        hasTodos={todosState.todos.length > 0}
-      />
-      <div className={styles.body}>
-        <div className={styles.header}>
-          <Button variant="delete" onClick={() => {}} disabled={todosState.todos.length > 0}>
-            Delete comleted Todos
-          </Button>
-        </div>
-        <TodoList
-          todos={todosState.todos}
-          onEditTodo={() => {}}
-          onChangeTodoStatus={() => {}}
-          onDeleteTodo={() => {}}
+    <>
+      <section className={styles.container}>
+        <TodoHeader
+          onAddTodo={openModal}
+          onDeleteAllTodos={() => {}}
+          hasTodos={todosState.todos.length > 0}
         />
-      </div>
-    </div>
+        <div className={styles.body}>
+          <div className={styles.header}>
+            <Button variant="delete" onClick={() => {}} disabled={todosState.todos.length > 0}>
+              Delete comleted Todos
+            </Button>
+          </div>
+          <TodoList
+            todos={todosState.todos}
+            onEditTodo={() => {}}
+            onChangeTodoStatus={() => {}}
+            onDeleteTodo={() => {}}
+          />
+        </div>
+      </section>
+      {isOpenModal && (
+        <TodoModal initialTodo={editingTodo} onSave={handleSave} onClose={closeModal} />
+      )}
+    </>
   );
 }
