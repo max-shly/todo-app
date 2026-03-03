ecdc18ce0dd8a4643c532ce965a21f33559add54
max-shly <maxschly@gmail.com>
2026-03-03 18:39:42 +0300

    feat: add TodoList component

---
+++ src/features/todo/components/TodoList/TodoList.tsx
@@ -0,0 +1,30 @@
+import { TodoItem } from '../TodoItem/TodoItem';
+
+import styles from './TodoList.module.less';
+
+import type { Todo } from '../../types';
+
+interface TodoListProps {
+  todos: Todo[];
+  onEditTodo: (todo: Todo) => void;
+  onChangeTodoStatus: (todo: Todo) => void;
+  onDeleteTodo: (id: string) => void;
+}
+
+export function TodoList({ todos, onEditTodo, onChangeTodoStatus, onDeleteTodo }: TodoListProps) {
+  if (todos.length === 0) return <p className={styles.empty}>No Todos</p>;
+
+  return (
+    <div className={styles.list}>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onEditTodo={onEditTodo}
+          onChangeTodoStatus={onChangeTodoStatus}
+          onDeleteTodo={onDeleteTodo}
+        />
+      ))}
+    </div>
+  );
+}
