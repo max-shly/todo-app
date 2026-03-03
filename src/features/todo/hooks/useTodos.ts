5cfc63978f7ad2dbceae224d9e22283e65bf1821
max-shly <maxschly@gmail.com>
2026-03-03 22:03:59 +0300

    feat: extend useTodos with addTodo method

--- src/features/todo/hooks/useTodos.ts
+++ src/features/todo/hooks/useTodos.ts
@@ -5,6 +5,8 @@ import toast from 'react-hot-toast';
 import { todoApi } from '../api/todoApi';
 import { todosReducer, initialState } from '../store/todosReducer';
 
+import type { Todo } from '../types';
+
 export function useTodos() {
   const [state, dispatch] = useReducer(todosReducer, initialState);
 
@@ -17,13 +19,26 @@ export function useTodos() {
       if (error) {
         dispatch({ type: 'FETCH_ERROR', payload: { error } });
         toast.error(error);
-      } else {
-        dispatch({ type: 'FETCH_SUCCESS', payload: { todos: data } });
       }
+
+      dispatch({ type: 'FETCH_SUCCESS', payload: { todos: data } });
+      toast.success('Todos loaded!');
     };
 
     loadTodos();
   }, []);
 
-  return { state };
+  const addTodo = async (todo: Omit<Todo, 'id' | 'createdAt' | 'updatedAt'>) => {
+    const { data, error } = await todoApi.createTodo(todo);
+
+    if (error) {
+      dispatch({ type: 'FETCH_ERROR', payload: { error } });
+      toast.error(error);
+    }
+
+    dispatch({ type: 'SET_TODOS', payload: { todos: data } });
+    toast.success('Todo added!');
+  };
+
+  return { state, addTodo };
 }
