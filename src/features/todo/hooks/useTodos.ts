26a17a87148da9ce5c96e6eae9ed1c9e90d81c8c
max-shly <maxschly@gmail.com>
2026-03-07 19:45:21 +0300

    refactor(useTodos): make toggleTodoStatus async for consistency

--- src/features/todo/hooks/useTodos.ts
+++ src/features/todo/hooks/useTodos.ts
@@ -69,13 +69,13 @@ export function useTodos() {
   }, []);
 
   const toggleTodoStatus = useCallback(
-    (todo: Todo) => {
+    async (todo: Todo) => {
       const toggled = {
         ...todo,
         status: (todo.status === 'complete' ? 'incomplete' : 'complete') as TodoStatus,
       };
 
-      updateTodo(toggled);
+      await updateTodo(toggled);
     },
     [updateTodo]
   );
